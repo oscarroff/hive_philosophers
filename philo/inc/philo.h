d370f3049a2420c03945d9f36f7eac50d5683512
Thomas Black-Roff <thblack-@c2r5p2.hive.fi>
2025-11-27 14:33:02 +0200

    build: initial file layout

---
+++ philo/inc/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thblack- <thblack-@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/25 18:07:57 by thblack-          #+#    #+#             */
+/*   Updated: 2025/10/06 18:26:07 by thblack-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef philo_H
+# define philo_H
+
+# include "libft.h"
+
+// CODES FOR ERROR TRACKING
+// SUCCESSFUL EXECUTION
+# define SUCCESS 1
+// UNSUCCEFUL EXECUTION
+# define FAIL 0
+
+// philo
+
+#endif
